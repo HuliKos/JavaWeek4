7d5c280bdc777305ead0ca1c10d8b3e790f7a3a6
Lan Kostrikin <lankostrikin@gmail.com>
2023-02-05 20:08:30 -0600

    Wulala

---
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,37 @@
+package com.devmountain.noteApp.entities;
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public Note(NoteDto noteDto){
+        if (noteDto.getBody() != null){
+            this.body = noteDto.getBody();
+        }
+    }
+
+}
